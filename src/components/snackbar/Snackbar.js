dc7fe7f6c6217bb89fde4047a5e4b2a55a01614b
Prana Apsara Wijaya <1911510996@student.budiluhur.ac.id>
2022-04-21 08:08:10 +0700

    add Snackbar.js and Spinner.js

---
+++ src/components/snackbar/Snackbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Snackbar = () => {
+  return <div>Snackbar</div>;
+};
+
+Snackbar.propTypes = {
+  //
+};
+
+export default Snackbar;
---
+++ src/components/snackbar/Snackbar.stories.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import Snackbar from "./Snackbar";
+
+storiesOf("<Snackbar />", module).add("Snackbar", () => (
+  <div>
+    <Snackbar />
+  </div>
+));
