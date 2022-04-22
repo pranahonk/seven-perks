d506a18b1050d8dd26e53c09692408d3064b98c8
prana.wijaya <prana.wijaya@mncgroup.com>
2022-04-22 13:15:36 +0700

    search display

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -1,18 +1,23 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import "./Search.scss";
 import Search from "../../assets/images/search-icon.png"
 
 const SearchBox = () => {
+    const [value, setValue] = useState("");
 
-  return (
-      <form className="search-container" action="//llamaswill.tumblr.com/search">
-        <input id="search-box" type="text" placeholder="Search all news" className="search-box" name="q"/>
-        <label htmlFor="search-box">
-            <img alt="search logo" className="search-icon" src={Search}/>
-        </label>
-        <input type="submit" id="search-submit"/>
-      </form>
-  );
+    useEffect(()=>{
+        console.log(value)
+    }, [value])
+
+    return (
+          <form className="search-container" action="//llamaswill.tumblr.com/search">
+            <input id="search-box" type="text" placeholder="Search all news" className="search-box" name="q" value={value} onChange={e => setValue(e.target.value)} />
+            <label htmlFor="search-box">
+                <img alt="search logo" className="search-icon" src={Search}/>
+            </label>
+            <input type="submit" id="search-submit"/>
+          </form>
+    );
 };
 
 
